9a55d4a2afef3c91dda991a16b741cc386e98c42
Andrew <andrew@macroconscious.com>
2017-01-08 14:37:47 -0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-$('div.nav-button.popin-button-header').on("click", function() {
+$("div.nav-button.popin-button-header").on("click", function() {
   $( "div.ajax-container" ).toggleClass( "shade" );
   alert("hey!");
-};
+});
