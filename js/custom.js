16d3a8653316e5253a8a509e4406002755a89af1
Andrew <andrew@macroconscious.com>
2017-03-08 12:44:49 -0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,4 @@
 $("div.mask.mask-inner ul.list li").on("click", function() {
+  console.log('clicked!')
   $('.main-header popin-header animation-start animation-start-timeout displayed').removeClass('displayed');
 });
