2df0a0f34f537d2d9b67a72f6a267fff5219bc59
Andrew <andrew@macroconscious.com>
2017-03-08 11:37:58 -0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,3 @@
-$("dic.mask.mask-outer ul.list li").on("click", function() {
+$("div.mask.mask-inner ul.list li").on("click", function() {
   $('.main-header popin-header animation-start animation-start-timeout displayed').removeClass('displayed');
 });
