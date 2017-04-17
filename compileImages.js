5ba2ba475578a776ca47b0c0ab1a65a6e293c6cd
Andrew Erickson <andrew@macroconscious.com>
2017-04-16 17:44:38 -0700

    build list of images from directory contents

---
+++ compileImages.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const imageDirectory = 'img';
+const outputFilename = 'images.json';
+
+function buildJSON(error, data) {
+  if (error) {
+    throw error;
+  }
+  const images = data.map(img => {
+    return path.join('/', imageDirectory, '/', img);
+  });
+
+  fs.writeFile(outputFilename, JSON.stringify(images));
+}
+
+// read directory, build JSON with info about files.
+fs.readdir(imageDirectory, buildJSON);
+
+console.log('Images successfully compiled to', outputFilename);
